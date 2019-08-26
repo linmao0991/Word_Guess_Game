bcf7eea9c3262023335e7494fd3bbd46e24a3f68
linmao0991 <lin.maoxian0991@gmail.com>
2019-08-25 23:40:15 -0400

    Function to display word place holder into the HTML.
    displayState function was added to game.js. The function loops through the randomly selected US State and displays a underscore for each letter in the state. inside the loop is a if statement looking for spaces to place a space in the html instead of a underscore.
    After the if statement a span is crated with two attributes id and data-letter, both set to the letter currently in the loop. Then the element is appeneded to the element with class selectedState

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,30 @@
 document.addEventListener('DOMContentLoaded', function(){
 
+//stores the array created from the randomly selected states letters.
 var selectedState = "";
+//stores the fact that corresponds with the randomly selected state.
 var selectedFact = "";
+//stores the number of attempts based on the letter count of each state.
 var totalAttempts = 0;
 var gameWins = 0;
 var gameLoss =0;
 
-// Object to store US States in an array and facts in an array
+// Object to store US States in an array and facts in an array.
 var gameData = {
-    // US States array
-    allStates: ["Ohio", "California", "Texas", "Florida","Hawaii", "New York", "Georgia"],
+    // Array containing the states to randomly choose from.
+    allStates: ["OHIO", "CALIFORNIA", "TEXAS", "FLORIDA","HAWAII", "NEW YORK", "GEORGIA"],
     // Facts array, facts index matches the states index.
-    stateFact: ["First to have traffic lights", "This State's flag has a bear on it", "Also known as the Lone Star State", "This state is the location of Disney World", "Only US state seperated by an ocean", "Also known as the Big Apple","This state was named after a king of England"]
+    stateFact: ["First to have traffic lights", "This State's flag has a bear on it", 
+                "Also known as the Lone Star State", "This state is the location of Disney World", 
+                "Only US state seperated by an ocean", "Also known as the Big Apple",
+                "This state was named after a king of England"],
+    // Array of the alpha used for validation of inputs and states with spaces
+    validLetters: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 }
 
+//Object that contains all of our game functions
 var gameFunctions = {
-
-    //randomState function selects state from the gameData object and stores necessary information into variables used for the game.
+    //This function grabs the data from the gameData object and stores them into the global variables to use in other functions.
     randomState: function(){
         var gameDataIndex = Math.floor(Math.random() * gameData.allStates.length);
         //randomly selects a state and converts into an array and store into variable selectedState.
@@ -27,11 +35,43 @@ var gameFunctions = {
         totalAttempts = selectedState.length + 5;
         //prints facts about the state to the <p> with ID stateFact in the HTML code.
         document.getElementById("stateFact").innerHTML = selectedFact;
-    }
+    },
+
+    //This function creates dashes in place of each letter of the random state selected and adds them to the html code.
+    displayState: function(){
+        //for loop to create a dash placeholder for each letter.
+        for (var i = 0; i < selectedState.length; i++){
+            var letterHolder = "";
+            var letterDash = "";
+
+            //Checks current index for a space
+            if (selectedState[i] === " "){ //If True
+                //Creates a <span> element
+                letterHolder = document.createElement("SPAN");
+                //creates a space and stores it into letterDash
+                letterDash = document.createTextNode("\xa0");
+            }
+            else{ //If False
+            //creates a <span> element and sets letterHolder to it.
+            letterHolder = document.createElement("SPAN");
+            //sets attribute id of created element to value of the index.
+            letterHolder.setAttribute("id", selectedState[i]);
+            letterHolder.setAttribute("data-letter", selectedState[i]);
+            //creates text and stores it into letterDash
+            letterDash = document.createTextNode("_ ");
+            }
+            //stores the text in letterDash into the <span> element in letterHolder
+            letterHolder.appendChild(letterDash);
+            //appends the <span> with text and inserts it into the element with id selectedState
+            document.getElementById("selectedState").appendChild(letterHolder);
+        }
+    },
+
 
 }
 
 gameFunctions.randomState();
+gameFunctions.displayState();
 console.log(selectedState);
 console.log(selectedFact);
 console.log(totalAttempts);
