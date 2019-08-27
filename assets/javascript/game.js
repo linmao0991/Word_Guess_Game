9b52221662f0a8ef73a7731ecc105c2ee207a9a2
linmao0991 <lin.maoxian0991@gmail.com>
2019-08-27 00:14:21 -0400

    Added 1 function, 3 event listeners
    onGuessInput function compares user key input and checks if input is a letter, if not then alert user to press a input a letter. Then checks if input is in the US State and Not a repeat incorrect guess, if input is not in the State and is not a repeat incorrect input then it will display incorrect guess on screen, add to incorrect guess array, and reduce total attemps by 1. The function still needs a conditional to check for correct letter input and display the letter in the letter holders.
    
    Added and eventlistener to listen for keypress and pass the keypress to the onGuessInput function then update the total attempt number.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,10 @@
 document.addEventListener('DOMContentLoaded', function(){
-
 //stores the array created from the randomly selected states letters.
 var selectedState = "";
 //stores the fact that corresponds with the randomly selected state.
 var selectedFact = "";
+//Array for incorrect letter guesses
+var incorrectGuessArray = [];
 //stores the number of attempts based on the letter count of each state.
 var totalAttempts = 0;
 var gameWins = 0;
@@ -19,7 +20,7 @@ var gameData = {
                 "Only US state seperated by an ocean", "Also known as the Big Apple",
                 "This state was named after a king of England"],
     // Array of the alpha used for validation of inputs and states with spaces
-    validLetters: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    validLetters: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 }
 
 //Object that contains all of our game functions
@@ -56,6 +57,7 @@ var gameFunctions = {
             letterHolder = document.createElement("SPAN");
             //sets attribute id of created element to value of the index.
             letterHolder.setAttribute("id", selectedState[i]);
+            //sets arttribute id of data-letter with value equal to value of the current index
             letterHolder.setAttribute("data-letter", selectedState[i]);
             //creates text and stores it into letterDash
             letterDash = document.createTextNode("_ ");
@@ -67,12 +69,60 @@ var gameFunctions = {
         }
     },
 
+    //Function that checks user key input against the letters of the US State.
+    onGuessInput: function(storedLetter){
+        //converts storedLetter to uppercase
+        var storedLetter = storedLetter.toUpperCase();
+        // declaring variables
+        var incorrectLetter = "";
+        var incorrectHolder = "";
+
+        //If function to check if key pressed is NOT a letter
+        if(!gameData.validLetters.includes(storedLetter)){
+            alert("Your input was not a letter, please try again!");
+        }
+
+        //If function to check is key pressed is NOT in selectedState array AND is in array validLetters AND NOT in incorrectGuessArray
+        if (!selectedState.includes(storedLetter) && gameData.validLetters.includes(storedLetter) && !incorrectGuessArray.includes(storedLetter)){
+            //Adds incorrect letter to the incorrectGuessArray. Stops this if condition to run if the incorrect letter was already guessed.
+            incorrectGuessArray.push(storedLetter);
+            //creates a span element and stores it into incorrectHolder
+            incorrectHolder = document.createElement("SPAN");
+            //creates text of stored letter with a space at the end and stors it into incorrectLetter.
+            incorrectLetter = document.createTextNode(storedLetter+" ");
+            //adds text in incorrectLetter into the span created in incorrectHolder
+            incorrectHolder.appendChild(incorrectLetter);
+            //Adds the created span with text into the HTML element with the id incorrectLetters.
+            document.getElementById("incorrectLetters").appendChild(incorrectHolder);
+            //Decrease totalAttempts by 1
+            totalAttempts--;
+            console.log(incorrectGuessArray);
+        }
+        //If function to check if letter is in the US State array selectedState
+        if (selectedState.includes(storedLetter)){
+            console.log("correct letter");
+        }
+
+    }
+
 
 }
-
+//Run gameFunctions to start game.
 gameFunctions.randomState();
 gameFunctions.displayState();
+
+//Event listener for keypress, function calls onGuessInput function passing through the key pressed.
+document.addEventListener("keypress", function(userKeyPress){
+    gameFunctions.onGuessInput(userKeyPress.key);
+    document.getElementById("attempsLeft").innerHTML = totalAttempts;
+});
+
+//Event listener to continually update attempsLeft with the current value of totalAttempts
+document.getElementById("attempsLeft").innerHTML = totalAttempts;
+
 console.log(selectedState);
 console.log(selectedFact);
 console.log(totalAttempts);
+console.log(incorrectGuessArray);
+console.log("total attemps" +totalAttempts);
 });
